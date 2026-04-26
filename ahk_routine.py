e788b87aa6ab53355758c1ffc3badf54f19a79f1
Kyle <kyleito@lbl.gov>
2026-04-25 17:57:14 -0700

    need to finish run row

--- ahk_routine.py
+++ ahk_routine.py
@@ -1,3 +1,5 @@
+from gc import get_count
+
 import numpy as np
 import ahk
 from ahk import AHK
@@ -6,10 +8,11 @@ from numpy.f2py.auxfuncs import throw_error
 
 
 class ahk_routine:
-    def __init__(self,file_name = r"C:\Users\USER1\PycharmProjects\keyenceAHK\VK_automation.xlsx"):
+    def __init__(self,file_name = r"C:\Users\USER1\PycharmProjects\keyenceAHK\VK_automation.xlsx",variableDictionary = {}):
         self.file_name = file_name
         self.ahk = ahk.ahk()
         self.df = pd.read_excel(self.file_name)
+        self.variableDictionary = variableDictionary
 
     #     check file format for correct headers
         valid_headers = all(
@@ -26,13 +29,18 @@ class ahk_routine:
             row = self.df.iloc[i]
             self.run_row(row)
 
+    # todo: make run individual row by reading row and executing actions
     def run_row(self,row):
+        # takes a single row from excel and runs it in ahk
+
         # find window
-        curr_win = ahk.find_window(title=row.Window)
+        win_name = check_for_variable(row.Window,variableDictionary=self.variableDictionary)
+        curr_win = ahk.find_window(title=win_name)
         if row.Mandatory & (curr_win == None):
             throw_error("windowNotFound")
-        # get control
 
+        # get control
+        curr_ctrl = self.get_control_from_window(curr_win, row.ClassNN)
 
     def get_control_from_window(self,window,classNN):
         # finds a control in a window  with control_class == classNN
@@ -46,6 +54,8 @@ class ahk_routine:
             return None
 
 def check_for_variable(term,variableDictionary):
+    # if "var varName" is inputted in excel, returns varName
+    # otherwise just returns the input
     if 'var ' in term:
         key = term.split()[1]
         print(key)
